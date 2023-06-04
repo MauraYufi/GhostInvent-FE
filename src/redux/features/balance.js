12c40e50343f12d19424c4228597717ea42e8c19
ciciliawidhiii <ciciliawidhi1@gmail.com>
2023-06-04 14:42:58 +0700

    add balance

---
+++ src/redux/features/balance.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
+const API_URL = `${BACKEND_URL}/api/balances/`;
+
+// Get all balances
+const getBalances = async () => {
+    const response = await axios.get(API_URL);
+    return response.data;
+  };
+
+const balance = {
+    getBalances
+};
+  
+export default productService;
